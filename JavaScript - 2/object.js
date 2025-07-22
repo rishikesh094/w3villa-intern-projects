2719127c2ca7298254182a26afc75c80037e829f
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-07-22 18:45:06 +0530

    Learn Object

--- JavaScript - 2/object.js
+++ JavaScript - 2/object.js
@@ -1,1 +1,13 @@
+//access dynamic property key 
 
+const user = {
+  name: "Aman", 
+  age: 21, 
+  place: "Noida"
+};
+
+let key = "age";
+
+console.log(user[key]);
+
+//when we change the key variable then access the object key dynamically 

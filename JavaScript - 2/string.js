c509d144ba71edd397f69beab4cd963215772b18
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-07-22 18:22:30 +0530

    Program using String methods

--- JavaScript - 2/string.js
+++ JavaScript - 2/string.js
@@ -1,1 +1,22 @@
+//validate email using string methods 
 
+let emailInput = "  Example123@gmail.com   ";
+
+let validEmail = emailInput.trim();
+// "Example123@gmail.com"
+
+validEmail = validEmail.toLowerCase();
+// "example123@gmail.com"
+
+// Remove spaces added before or after "@"
+validEmail = validEmail.replace(/\s*@\s*/g, "@");
+
+if (!validEmail.endsWith(".com")) {
+  validEmail += ".com";
+}
+
+//remove multiple dots
+validEmail = validEmail.replace(/\.{2,}/g, ".");
+
+
+console.log("Valid Email: ", validEmail);

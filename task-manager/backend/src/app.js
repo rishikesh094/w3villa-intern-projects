53a3ac4f2e949ccdc9095f3f6d71d9e42d64cb3d
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-09-05 17:45:36 +0530

    Update app.js

--- task-manager/backend/src/app.js
+++ task-manager/backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import taskRoutes from "./routes/task.routes.js";
@@ -12,6 +13,12 @@ const app = express();
 // Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// CORS configuration
+app.use(cors({
+  origin: "https://task-manager094.netlify.app", 
+  credentials: true
+}));
 
 
 // API routes

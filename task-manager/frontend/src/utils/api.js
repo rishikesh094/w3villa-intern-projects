1c1589a683f3b9a205ff2c00c4ffbee1ad01c077
Rishikesh Singh <149285390+rishikesh094@users.noreply.github.com>
2025-09-05 16:50:14 +0530

    Update api.js

--- task-manager/frontend/src/utils/api.js
+++ task-manager/frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4040/api';
+const API_BASE_URL = 'https://task-manager-backend-2bmv.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
